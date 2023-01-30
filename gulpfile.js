65b5877b43fe93b335a5e6cf2c6e4b361208c191
VladyslavDykyi <vladislavdykyi33@gmail.com>
2023-01-30 17:09:57 +0200

    create project, add fonts and fonts settings

--- gulpfile.js
+++ gulpfile.js
@@ -134,15 +134,15 @@ gulp.task('svgSprite', function () {
 function fontsStyle(params) {
 
 	let file_content = fs.readFileSync(source_folder + '/scss/default/_fonts.scss');
-	if (file_content == '') {
-		fs.writeFile(source_folder + '/scss/default/fonts.scss', '', cb);
+	if (file_content === '') {
+		fs.writeFile(source_folder + '/scss/default/_fonts.scss', '', cb);
 		return fs.readdir(path.build.fonts, function (err, items) {
 			if (items) {
 				let c_fontname;
-				for (var i = 0; i < items.length; i++) {
+				for (let i = 0; i < items.length; i++) {
 					let fontname = items[i].split('.');
 					fontname = fontname[0];
-					if (c_fontname != fontname) {
+					if (c_fontname !== fontname) {
 						fs.appendFile(source_folder + '/scss/default/_fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
 					}
 					c_fontname = fontname;
