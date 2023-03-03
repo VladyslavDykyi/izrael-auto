05925f9f856892ac78a53c45dd35dc6de3ec10c9
VladyslavDykyi <vladislavdykyi33@gmail.com>
2023-03-03 02:54:59 +0200

    lk chat, messages, lk fix

---
+++ #src/js/pages/lk-chat.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('.input-file input');
+const fileName = document.querySelector('.file-name');
+input.addEventListener('input',handleFiles,false);
+
+function handleFiles() {
+	const fileList = this.files;
+	let i = 0
+	for (; i < fileList.length; i++) {
+		const item = document.createElement('li');
+		item.classList.add('t-regular','t-4','s');
+		item.textContent = `${fileList[i].name}`;
+		fileName.append(item);
+	}
+}
