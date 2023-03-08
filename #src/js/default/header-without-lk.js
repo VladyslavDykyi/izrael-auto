04f858f35b654587e590c4259dccd201d1674ca7
VladyslavDykyi <vladislavdykyi33@gmail.com>
2023-03-08 23:29:42 +0200

    fixes, and lk creating car ad

---
+++ #src/js/default/header-without-lk.js
@@ -0,0 +1,131 @@
+
+// скрол для шапки
+if (window.innerWidth >= 1024) {
+// откритие випадашки еще
+	openDropMenu();
+
+	function openDropMenu() {
+		const btn = document.querySelector('.else > .drop-btn');
+		const btn2 = document.querySelector('.lang > .drop-btn');
+		const btn3 = document.querySelector('.header-buttons-user > .drop-btn');
+		const toggleActive = () => {
+			btn2.classList.remove('active');
+			btn3.classList.remove('active');
+			btn.classList.toggle('active');
+		}
+		btn.addEventListener('click', e => {
+			e.stopPropagation();
+			toggleActive();
+		});
+		document.addEventListener('click', e => {
+			let target = e.target;
+			let its_menu = target === btn || btn.contains(target);
+			let its_btn = target === btn;
+			let menu_is_active = btn.classList.contains('active');
+			if (!its_menu && !its_btn && menu_is_active) {
+				toggleActive();
+			}
+		});
+	}
+
+	openDropMenu2();
+
+	function openDropMenu2() {
+		const btn = document.querySelector('.lang > .drop-btn');
+		const btn2 = document.querySelector('.else > .drop-btn');
+		const btn3 = document.querySelector('.header-buttons-user > .drop-btn');
+		const toggleActive = () => {
+			btn2.classList.remove('active');
+			btn3.classList.remove('active');
+			btn.classList.toggle('active');
+		}
+		btn.addEventListener('click', e => {
+			e.stopPropagation();
+
+			toggleActive();
+		});
+		document.addEventListener('click', e => {
+			let target = e.target;
+			let its_menu = target === btn || btn.contains(target);
+			let its_btn = target === btn;
+			let menu_is_active = btn.classList.contains('active');
+			if (!its_menu && !its_btn && menu_is_active) {
+				toggleActive();
+			}
+		});
+	}
+
+	openDropMenu3();
+
+	function openDropMenu3() {
+		const btn = document.querySelector('.header-buttons-user > .drop-btn');
+		const btn2 = document.querySelector('.lang > .drop-btn');
+		const btn3 = document.querySelector('.else > .drop-btn');
+		const toggleActive = () => {
+			btn2.classList.remove('active');
+			btn3.classList.remove('active');
+			btn.classList.toggle('active');
+		}
+		btn.addEventListener('click', e => {
+			e.stopPropagation();
+			toggleActive();
+		});
+		document.addEventListener('click', e => {
+			let target = e.target;
+			let its_menu = target === btn || btn.contains(target);
+			let its_btn = target === btn;
+			let menu_is_active = btn.classList.contains('active');
+			if (!its_menu && !its_btn && menu_is_active) {
+				toggleActive();
+			}
+		});
+	}
+}
+// відкриття випадашки еще кінець
+if (window.innerWidth <= 1024) {
+// мобілка язик початок
+	const container = document.querySelector('.lang-text');
+	const menu = document.querySelector('.lang-drop-mob');
+
+	const toggleActive = () => {
+		container.classList.toggle('active');
+	}
+	container.addEventListener('click', e => {
+		e.stopPropagation();
+		toggleActive();
+	});
+	document.addEventListener('click', e => {
+		let target = e.target;
+		let its_menu = target === menu || menu.contains(target);
+		let its_btn = target === container;
+		let menu_is_active = container.classList.contains('active');
+
+		if (!its_menu && !its_btn && menu_is_active) {
+			toggleActive();
+		}
+	});
+// мобілка язик кінець
+// мобілка burger початок
+	const burger = document.querySelector('.header-burger button');
+	const headerMob = document.querySelector('.header-mob');
+	const body = document.querySelector('body');
+	const toggleBurgerActive = () => {
+		body.classList.toggle('lock');
+		headerMob.classList.toggle('active');
+	}
+	burger.addEventListener('click', e => {
+		e.stopPropagation();
+		toggleBurgerActive();
+	});
+	document.addEventListener('click', e => {
+		let target = e.target;
+		let its_menu = target === headerMob || headerMob.contains(target);
+		let its_btn = target === burger;
+		let menu_is_active = headerMob.classList.contains('active');
+
+		if (!its_menu && !its_btn && menu_is_active) {
+			toggleBurgerActive();
+		}
+	});
+// мобілка burger кінець
+}
---
+++ dist/js/pages/lk-creating-car-ad.js
@@ -0,0 +1,108 @@
+function tippyUse() {
+	tippy('[data-tippy-content]',
+		{
+			placement: 'top',
+			arrow: true,
+			theme: 'light-blueBorder',
+			duration: 0,
+			delay: [700, 500],
+			dynamicTitle: true,
+		});
+}
+
+tippyUse();
+
+$('.select-no-search').select2({
+	minimumResultsForSearch: -1,
+	theme: "search",
+});
+$('.select-search').select2({
+	theme: "search",
+});
+// $('#demo').daterangepicker({
+// 	ranges: {
+// 		'Today': [moment(), moment()],
+// 		'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+// 		'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+// 		'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+// 		'This Month': [moment().startOf('month'), moment().endOf('month')],
+// 		'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+// 	},
+// 	"alwaysShowCalendars": true,
+// 	"startDate": "03/01/2023",
+// 	"endDate": "03/07/2023"
+// }, function(start, end, label) {
+// 	console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+// });
+$('.inspection-up-to').daterangepicker({
+	"singleDatePicker": true,
+	"minYear": 2023,
+	"alwaysShowCalendars": true,
+	"startDate": "03/01/2023",
+	"endDate": "03/07/2023",
+	"minDate": "03/07/2023"
+}, function (start, end, label) {
+	console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+});
+window.addEventListener("DOMContentLoaded", function () {
+	[].forEach.call(document.querySelectorAll('.tel'), function (input) {
+		function mask(event) {
+			let keyCode;
+			event.keyCode && (keyCode = event.keyCode);
+			let pos = this.selectionStart;
+			if (pos < 1) event.preventDefault();
+			let matrix = "+(___) _________",
+				i = 0,
+				def = matrix.replace(/\D/g, ""),
+				val = this.value.replace(/\D/g, ""),
+				new_value = matrix.replace(/[_\d]/g, function (a) {
+					return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+				});
+			i = new_value.indexOf("_");
+			if (i != -1) {
+				i < 1 && (i = 2);
+				new_value = new_value.slice(0, i);
+			}
+			let reg = matrix.substr(0, this.value.length).replace(/_+/g,
+				function (a) {
+					return "\\d{1," + a.length + "}"
+				}).replace(/[+()]/g, "\\$&");
+			reg = new RegExp("^" + reg + "$");
+			if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+			if (event.type == "blur" && this.value.length < 5) this.value = "";
+		}
+		input.addEventListener("input", mask, false);
+		input.addEventListener("focus", mask, false);
+		input.addEventListener("blur", mask, false);
+		input.addEventListener("keydown", mask, false)
+	});
+	function fileFields(inp) {
+		const avatar = document.querySelectorAll(inp);
+		for (let i = 0; i < avatar.length; i++) {
+			avatar[i].addEventListener('input', function() {
+				let reader = new FileReader();
+				reader.onload = function (e) {
+					avatar[i].parentElement.nextElementSibling.lastElementChild.setAttribute('src', e.target.result);
+					avatar[i].parentElement.nextElementSibling.firstElementChild.srcset = e.target.result;
+					avatar[i].parentElement.previousElementSibling.value = e.target.result;
+				}
+				reader.readAsDataURL(this.files[0]);
+				avatar[i].parentElement.nextElementSibling.lastElementChild.classList.add('active');
+				avatar[i].parentElement.classList.add('none');
+				avatar[i].parentElement.parentElement.classList.add('active');
+				deleteFile(avatar[i].parentElement.nextElementSibling.nextElementSibling.firstElementChild);
+			});
+		}
+		function deleteFile(button) {
+			button.addEventListener('click', () => {
+				button.parentElement.previousElementSibling.firstElementChild.srcset = '';
+				button.parentElement.previousElementSibling.lastElementChild.src = '';
+				button.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.value = '';
+				button.parentElement.previousElementSibling.previousElementSibling.classList.remove('none');
+				button.parentElement.previousElementSibling.lastElementChild.classList.remove('active');
+				button.parentElement.parentElement.classList.remove('active');
+			});
+		}
+	}
+	fileFields('.file-inp');
+});
